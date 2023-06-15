0ebe72b0423e41493460495c23f736432b04c0a3
Iván González Dávila <ivangdavila@creatinium.com>
2023-06-15 17:37:03 +0200

    fix(ideas): Avoid news and links

--- generators/idea_generator.py
+++ generators/idea_generator.py
@@ -1,16 +1,16 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
-from utils import format_list, write_to_file
+from utils import format_list, add_item_to_file
 
 class IdeaGenerator:
-    def __init__(self, brand_info, ideas_per_topic):
+    def __init__(self, brand_info, number_of_ideas):
         self.gpt3 = ChatOpenAI(temperature=0.5)
         self.gpt4 = ChatOpenAI(temperature=0.5, model_name="gpt-4")
         self.brand_info = brand_info
-        self.ideas_per_topic = ideas_per_topic
+        self.number_of_ideas = number_of_ideas
 
     def generate_ideas(self, topic):
-        topic_prompt = f"Create a list of {self.ideas_per_topic} social media post ideas (concise and specific) for their account about the topic '{topic}' in the format '- Text for first idea....\n- Text of second...'"
+        topic_prompt = f"Create a list of {self.number_of_ideas} social media post ideas (concise and specific) for their account about the topic '{topic}' in the format '- ...\n- ...\n\nNote: avoid ideas that require up-to-date information, and avoid ideas that depend on a real link or offered product/service'"
         ideas = [
             i.replace("- ", "")
             for i in self.gpt3(
@@ -19,7 +19,8 @@ class IdeaGenerator:
             .content.strip()
             .split("\n")
             if len(i) > 2
-        ]
+        ][: self.number_of_ideas]
         print("Generated ideas:\n\n", format_list(ideas), "\n\n---------\n")
-        write_to_file("results/ideas.txt", '\n'.join(ideas), mode='a')
+        for idea in ideas:
+            add_item_to_file("results/ideas.txt", idea)
         return ideas
