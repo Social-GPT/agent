6cd8dcc80645bccb36e0b25415b38629339d2fbc
Iván González Dávila <ivangdavila@creatinium.com>
2023-06-15 04:29:25 +0200

    feat(facebook): Added instagram posts support

---
+++ generators/instagram_generator.py
@@ -0,0 +1,19 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+from utils import write_to_file
+
+class InstagramGenerator:
+    def __init__(self, brand_info, idea):
+        self.gpt3 = ChatOpenAI(temperature=0.5)
+        self.gpt4 = ChatOpenAI(temperature=0.5, model_name="gpt-4")
+        self.brand_info = brand_info
+        self.idea = idea
+
+    def generate_post(self):
+        idea_prompt = f"Write an Instagram post for his account that talks about '{self.idea}'"
+        post = self.gpt4(
+            [SystemMessage(content=self.brand_info), HumanMessage(content=idea_prompt)]
+        ).content.strip()
+        print("Generated Instagram post:\n\n", post, "\n\n---------\n")
+        write_to_file("results/instagram.txt", post, mode='a')
+        return post
