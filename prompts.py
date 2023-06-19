d7613bba2e48952bf972ab148de82070dbb3e98c
Iván González Dávila <ivangdavila@creatinium.com>
2023-06-19 14:48:37 +0200

    feat(style): Build style prompt

--- prompts.py
+++ prompts.py
@@ -1,4 +1,5 @@
 from style import default_writting_style_definitions
+from brands import Brand
 
 class Prompts: 
     
@@ -6,5 +7,5 @@ class Prompts:
     def get_avoids():
         return "\n\nNote: avoid including any text or ideas which requires up-to-date information, or which could contain false data, or which mentions a real link or offered product/service"
     
-    def get_default_style():
-        return '\n\nFollow these style guidelines:' + ', '.join(default_writting_style_definitions)
+    def build_style_prompt(style_items: str):
+        return '\n\nFollow these style guidelines:' + ', '.join(style_items)
