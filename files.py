79095f7eca1436b4f4bce01050fb0997b60d4009
Iván González Dávila <ivangdavila@creatinium.com>
2023-06-19 14:45:14 +0200

    refactor(files): Filenames as separated class

---
+++ files.py
@@ -0,0 +1,10 @@
+class Files:
+    twitter_results = 'results/twitter.txt'
+    facebook_results = 'results/facebook.txt'
+    instagram_results = 'results/instagram.txt'
+    linkedin_results = 'results/linkedin.txt'
+    topic_results = 'results/topics.txt'
+    idea_results = 'results/ideas.txt'
+
+    brand_descriptions = 'cache/brand-descriptions.txt'
+    brand_styles = 'cache/brand-styles.txt'
