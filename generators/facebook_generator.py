c630812bb336abed070ee92b7decfd7976624ba5
Iván González Dávila <ivangdavila@creatinium.com>
2023-06-19 12:36:26 +0200

    feat(posts): Custom post style prompt expansion

--- generators/facebook_generator.py
+++ generators/facebook_generator.py
@@ -5,15 +5,16 @@ from utils import add_item_to_file
 class FacebookGenerator:
     def __init__(self, brand_info, language, idea, prompt_expansion):
         self.gpt3 = ChatOpenAI(temperature=0.5)
-        self.gpt4 = ChatOpenAI(temperature=0.5, model_name="gpt-4")
         self.brand_info = brand_info
         self.language = language
         self.idea = idea
         self.prompt_expansion = prompt_expansion
 
     def generate_post(self):
-        idea_prompt = f"Write a facebook post with 3-6 paragraphs in {self.language} for his account that talks about '{self.idea}'.\n\nNote: avoid including any text which requires up-to-date information, or which could contain false data, or which could contain false data, or which mentions a real link or offered product/service\n\nTake this also into account: {self.prompt_expansion}"
-        post = self.gpt4(
+        idea_prompt = f"Write a facebook post with 3-6 paragraphs in {self.language} for his account that talks about '{self.idea}'.\n\nNote: avoid including any text which requires up-to-date information, or which could contain false data, or which could contain false data, or which mentions a real link or offered product/service"
+        if (self.prompt_expansion != ""):
+            prompt = prompt + f"\n\nTake this also into account: {self.prompt_expansion}"
+        post = self.gpt3(
             [SystemMessage(content=self.brand_info), HumanMessage(content=idea_prompt)]
         ).content.strip()
         print("Generated Facebook post:\n\n", post, "\n\n---------\n")
--- main.py
+++ main.py
@@ -18,27 +18,26 @@ def main():
     ideas_per_topic = int(input("\nNumber of posts per topic?\n"))
     posts_language = input("\nLanguage of the posts?\n")
     brand_info = Brand.request_brand_info().description
-    topics_ideas_prompt_expansion = input("\nAny specific request about the TOPICS/IDEAS?\n")
-    posts_prompt_expansion = input("\nAny specific request about the style or content of the POSTS?\n")
+    topics_ideas_prompt_expansion = input("\nAny specific request about the TOPICS/IDEAS?\n") or ""
+    posts_prompt_expansion = input("\nAny specific request about the style or content of the POSTS?\n") or ""
     generate_images = ask_boolean("\nUse image generation feature (beta)?", False)
-    platforms = inquirer.prompt([inquirer.Checkbox('platforms', message="\nWhich platforms do you want to target??", choices=["Instagram", "Facebook", "Twitter"])])
+    platforms = inquirer.prompt([inquirer.Checkbox('platforms', message="\nWhich platforms do you want to target??", choices=["Instagram", "Facebook", "Twitter"])])['platforms']
 
     print('\n👍🏼 Nice! Started generating...\n')
 
-    topics = TopicGenerator(brand_info, topic_count).generate_topics(topics_ideas_prompt_expansion)
-
+    topics = TopicGenerator(brand_info, topic_count, topics_ideas_prompt_expansion).generate_topics()
         
 
     for topic in topics:
-        ideas = IdeaGenerator(brand_info, ideas_per_topic).generate_ideas(topic, topics_ideas_prompt_expansion)
+        ideas = IdeaGenerator(brand_info, ideas_per_topic, topics_ideas_prompt_expansion).generate_ideas(topic)
 
         for idea in ideas:
             if "Twitter" in platforms:
-                TweetGenerator(brand_info, posts_language, idea).generate_tweet(posts_prompt_expansion)
+                TweetGenerator(brand_info, posts_language, idea, posts_prompt_expansion).generate_tweet()
             if "Facebook" in platforms:
-                FacebookGenerator(brand_info, posts_language, idea).generate_post(posts_prompt_expansion)
+                FacebookGenerator(brand_info, posts_language, idea, posts_prompt_expansion).generate_post()
             if "Instagram" in platforms:
-                InstagramGenerator(brand_info, posts_language, idea).generate_post(posts_prompt_expansion)            
+                InstagramGenerator(brand_info, posts_language, idea, posts_prompt_expansion).generate_post()            
             if generate_images:
                 hf_api_token = os.environ.get("HUGGINGFACE_API_TOKEN")
                 if hf_api_token:
